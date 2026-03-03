8792964d29713ac5ea3a1d5f9fe53d6f62ad73f9
bertoarc <bertoarcpiq@gmail.com>
2026-03-03 21:18:42 +1100

    Add .gitkeep to public directory and dynamic product detail page to app/shop/[id]/page.tsx

---
+++ app/shop/[id]/page.tsx
@@ -0,0 +1,15 @@
+// Dynamic product detail page
+
+import React from 'react';
+
+const ProductDetail = ({ product }) => {
+    return (
+        <div>
+            <h1>{product.name}</h1>
+            <p>{product.description}</p>
+            <p>Price: ${product.price}</p>
+        </div>
+    );
+};
+
+export default ProductDetail;
